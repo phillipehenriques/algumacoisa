a73b7435ce0e944e69cdf0bc26c3d2829e4ff8b8
phillipe henriques <phillipe_ch@hotmail.com>
2022-10-04 15:19:19 -0300

    alteração no main.js

--- meu-projetinho/main.js
+++ meu-projetinho/main.js
@@ -1,23 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+user = {
+  nickname: 'pharanoia',
+  age: 30,
+  address: {
+      street: 'Rua porto feliz',
+      number: 243
+  },
+}; 
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Desestruturação 
 
-setupCounter(document.querySelector('#counter'))
+function mostrarIdade(user) {
+  return user.idade;
+}
+
+document.body.innerText = mostrarIdade(user)
